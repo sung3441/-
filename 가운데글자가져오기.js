244b88631d77e04b8a0f88d8fc029ae569514464
sung3441 <67699933+sung3441@users.noreply.github.com>
2021-05-04 23:50:09 +0900

    Create 가운데글자가져오기.js

---
+++ 가운데글자가져오기.js
@@ -0,0 +1,11 @@
+function solution(s) {
+    var answer = '';
+    var len = s.length;
+    if(len % 2 == 0){
+        answer = s.slice(len/2-1, len/2+1);
+    }else{
+        answer = s.slice(len/2, len/2+1);
+    }
+    
+    return answer;
+}

9d9bfe0fc4b6351fa55eea90bf8c4fdf160d4ab7
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2021-10-29 01:22:34 +0900

    Create 크레인 인형뽑기 게임.java

---
+++ 크레인 인형뽑기 게임.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        ArrayList list = new ArrayList();
+
+        int cnt = 0;
+        
+        for(int move : moves) 
+            boolean flag = true;
+            for(int i = 0; i < board.length; i++) {
+                if(board[i][move-1] != 0 && flag) {
+                    list.add(board[i][move-1]);
+                    board[i][move-1] = 0;
+                    flag = false;
+                    if(list.size() > 1) {                 
+                        if(list.get(list.size()-1) == list.get(list.size()-2)) {
+                            list.remove(list.size()-1);
+                            list.remove(list.size()-1);
+                            cnt += 2;
+                        }
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+}

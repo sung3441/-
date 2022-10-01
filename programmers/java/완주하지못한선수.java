2cb09d1bb3db6780ee04d3fe3fc2be662f99f3bb
chosungil <whtjddlf56@naver.com>
2022-10-01 23:53:01 +0900

    hash 방식 추가

--- programmers/java/완주하지못한선수.java
+++ programmers/java/완주하지못한선수.java
@@ -1,14 +1,14 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 class Solution {
-    public String solution(String[] participant, String[] completion) {
+    // sort
+    public String solution1(String[] participant, String[] completion) {
         String answer = "";
-        //완주하지 못한 사람의 이름을 담을 변수
-        String result = "";
-        
-        //참가자의 배열을 정렬
+
         Arrays.sort(participant);
-        //완주자의 배열을 정렬
         Arrays.sort(completion);
         
         for(int i = 0; i < completion.length; i++){
@@ -17,7 +17,50 @@ class Solution {
                 break;
             }
         }
-            
+        return answer;
+    }
+
+    // hash > keySet
+    public String solution2(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String player : participant) {
+            map.put(player, map.getOrDefault(player, 0) + 1);
+        }
+
+        for (String player : completion) {
+            map.put(player, map.get(player) - 1);
+        }
+
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0) {
+                answer = key;
+                break;
+            }
+        }
+        return answer;
+    }
+
+    // hash > entry
+    public String solution3(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String player : participant) {
+            map.put(player, map.getOrDefault(player, 0) + 1);
+        }
+
+        for (String player : completion) {
+            map.put(player, map.get(player) - 1);
+        }
+
+        Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry<String, Integer> entry = iter.next();
+            if (entry.getValue() != 0) {
+                answer = entry.getKey();
+                break;
+            }
+        }
         return answer;
     }
 }

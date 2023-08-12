b56f2831b6a8aa2cc7f0fd2988ec9281d26f2f9b
chosungil <whtjddlf56@naver.com>
2023-08-12 12:23:22 +0900

    add JadenCase 문자열 만들기

---
+++ programmers/java/JadenCase_문자열_만들기.java
@@ -0,0 +1,25 @@
+public class JadenCase_문자열_만들기 {
+
+    public String solution(String s) {
+        s = s.toLowerCase();
+        char[] chars = s.toCharArray();
+        // 65, 97
+
+        boolean flag = true;
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (c == ' ') {
+                flag = true;
+                continue;
+            }
+
+            if (c >= 'a' && c <= 'z' && flag) {
+                chars[i] = (char) (c - 32);
+            }
+
+            flag = false;
+        }
+
+        return String.valueOf(chars);
+    }
+}

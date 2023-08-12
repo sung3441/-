a4a550ac6c8a63372cec8aebb1364305a502e226
chosungil <csi@mlsoft.com>
2023-08-12 12:20:33 +0900

    add 카펫

---
+++ programmers/java/카펫.java
@@ -0,0 +1,17 @@
+public class 카펫 {
+
+    public int[] solution(int brown, int yellow) {
+        int sum = brown + yellow;
+        int col = 3;
+
+        for (; sum / col > 2; col++) {
+            int row = sum / col;
+            if (col * row == sum && brown == (row * 2) + (col * 2 - 4)) {
+                break;
+            }
+        }
+
+        int max = Math.max(col, sum / col);
+        return new int[]{max, sum / max};
+    }
+}
